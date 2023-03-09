89b542a80651f2d7c0efa3d2ab2f2813c4c1a500
MarcosAlexandreCoelhoCampos <marcooscampos@hotmail.com>
2023-03-09 18:51:38 -0300

    add functions

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { clickTeachers } from "./scripts/clickTeachers.js";
 import { clickSignUp } from "./scripts/clickSignUp.js";
+import { menu } from "./scripts/menu.js";
+import { assessments } from "./scripts/assessments.js";
 
 clickTeachers();
 clickSignUp();
+menu();
+assessments();
